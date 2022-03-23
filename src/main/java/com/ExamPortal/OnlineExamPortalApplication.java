daa977b65f6297c17ed4baf04edaafa227a9d721
veloxigami <anil.kumar.iiitm@gmail.com>
2022-03-23 19:22:21 +0530

    stranger commit

--- src/main/java/com/ExamPortal/OnlineExamPortalApplication.java
+++ src/main/java/com/ExamPortal/OnlineExamPortalApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineExamPortalApplication {
 
+	String meraname;
+
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineExamPortalApplication.class, args);
 	}
