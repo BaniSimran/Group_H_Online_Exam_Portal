8b7c6b68fd4fdb6505a79f81b1ccc4168b6b2cd5
veloxigami <anil.kumar.iiitm@gmail.com>
2022-04-17 21:07:25 +0530

    minor change

--- src/main/java/com/examportal/controller/WebViewController.java
+++ src/main/java/com/examportal/controller/WebViewController.java
@@ -12,52 +12,52 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class WebViewController {
-
-    @Autowired
-    OnlineExamService onlineExamService;
-
-    @GetMapping("/")
-    public String indexPage(Model model){
-        return "index";
-    }
-    @GetMapping(value = "/student-login")
-    public String studentLogin(Model model){
-        model.addAttribute("student", new Student());
-        return "studentLogin";
-    }
-    @GetMapping(value = "/register-student")
-    public String registerStudent(Model model){
-        model.addAttribute("student", new Student());
-        return "registerStudent";
-    }
-    @GetMapping(value = "/admin-login")
-    public String adminLogin(Model model){
-        model.addAttribute("admin", new Admin());
-        return "adminLogin";
-    }
-
-
-    @PostMapping(value = "/")
-    public String registerStudentSave(@ModelAttribute Student student, Model model){
-        Long id = onlineExamService.createNewStudent(student);
-        model.addAttribute("student",student);
-        model.addAttribute("id",id);
-        return "index";
-    }
-
-    @RequestMapping(value = "/student-dashboard", method = RequestMethod.POST)
-    public String studentDashboard(@ModelAttribute Student student, Model model){
-        StudentDTO std = onlineExamService.loginStudent(student);
-        model.addAttribute("student",std);
-        return "studentDashboard";
-    }
-
-    @RequestMapping(value = "/admin-dashboard", method = RequestMethod.POST)
-    public String adminDashboard(@ModelAttribute Admin admin, Model model){
-        AdminDTO std = onlineExamService.loginAdmin(admin);
-        model.addAttribute("admin",std);
-        return "adminDashboard";
-    }
+//
+//    @Autowired
+//    OnlineExamService onlineExamService;
+//
+//    @GetMapping("/")
+//    public String indexPage(Model model){
+//        return "index";
+//    }
+//    @GetMapping(value = "/student-login")
+//    public String studentLogin(Model model){
+//        model.addAttribute("student", new Student());
+//        return "studentLogin";
+//    }
+//    @GetMapping(value = "/register-student")
+//    public String registerStudent(Model model){
+//        model.addAttribute("student", new Student());
+//        return "registerStudent";
+//    }
+//    @GetMapping(value = "/admin-login")
+//    public String adminLogin(Model model){
+//        model.addAttribute("admin", new Admin());
+//        return "adminLogin";
+//    }
+//
+//
+//    @PostMapping(value = "/")
+//    public String registerStudentSave(@ModelAttribute Student student, Model model){
+//        Long id = onlineExamService.createNewStudent(student);
+//        model.addAttribute("student",student);
+//        model.addAttribute("id",id);
+//        return "index";
+//    }
+//
+//    @RequestMapping(value = "/student-dashboard", method = RequestMethod.POST)
+//    public String studentDashboard(@ModelAttribute Student student, Model model){
+//        StudentDTO std = onlineExamService.loginStudent(student);
+//        model.addAttribute("student",std);
+//        return "studentDashboard";
+//    }
+//
+//    @RequestMapping(value = "/admin-dashboard", method = RequestMethod.POST)
+//    public String adminDashboard(@ModelAttribute Admin admin, Model model){
+//        AdminDTO std = onlineExamService.loginAdmin(admin);
+//        model.addAttribute("admin",std);
+//        return "adminDashboard";
+//    }
 
 
 }
